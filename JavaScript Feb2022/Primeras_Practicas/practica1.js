16143fd41c6171c90caaa1ef628261d867981114
Yuri Reyes <saidayurishi@gmail.com>
2022-02-05 09:59:11 -0600

    agregando primera practica

---
+++ JavaScript Feb2022/Primeras_Practicas/practica1.js
@@ -0,0 +1,19 @@
+//Variables y operadores
+
+var a = 1;
+let b = 45;
+var c = 6;
+var d;
+d = a + b;
+
+console.log(d)
+console.log("Resultado de la variable d: ",d)
+console.log(a+b)
+console.log(8+5)
+console.log(a,b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+
+
